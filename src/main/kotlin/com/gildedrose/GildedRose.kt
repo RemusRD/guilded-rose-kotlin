330c460cee9328a0f11f5f73f6aa43a652bf7413
victor.cosqui <victor.cosqui@personio.de>
2022-02-11 17:15:48 +0100

    change naming and magic numbers removal

--- src/main/kotlin/com/gildedrose/GildedRose.kt
+++ src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -2,54 +2,60 @@ package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
 
+    private val MAX_QUALITY = 50
+
     fun updateQuality() {
         for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
+            val item = items[i]
+            if (item.name != "Aged Brie" && item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.quality > 0) {
+                    if (item.name != "Sulfuras, Hand of Ragnaros") {
+                        item.quality = item.quality - 1
                     }
                 }
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
+                if (item.quality < MAX_QUALITY) {
+                    item.quality = item.quality + 1
 
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                    if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.sellIn in 6..10) {
+                            if (item.quality < MAX_QUALITY) {
+                                item.quality = item.quality + 1
                             }
                         }
 
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
+                        if (item.sellIn < 6) {
+                            if (item.quality < MAX_QUALITY) {
+                                item.quality = item.quality + 2
                             }
                         }
                     }
                 }
             }
 
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
+            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                item.sellIn = item.sellIn - 1
             }
 
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
+            if (item.sellIn < 0) {
+                if (item.name != "Aged Brie") {
+                    if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
+                        if (item.quality > 0) {
+                            if (item.name != "Sulfuras, Hand of Ragnaros") {
+                                item.quality = item.quality - 1
                             }
                         }
                     } else {
-                        items[i].quality = items[i].quality - items[i].quality
+                        item.quality = 0
                     }
                 } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
+                    if (item.quality < MAX_QUALITY) {
+                        item.quality = item.quality + 1
                     }
                 }
+            }
+            if(item.quality>MAX_QUALITY) {
+                item.quality = MAX_QUALITY
             }
         }
     }
--- src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -6,13 +6,92 @@ import org.junit.jupiter.api.Test
 internal class GildedRoseTest {
 
     @Test
-    fun foo() {
-        val items = arrayOf<Item>(Item("foo", 0, 0))
+    fun decreaseSellIn() {
+        val items = arrayOf(Item("random product", 10, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
+        assertEquals(9, app.items[0].sellIn)
+        app.updateQuality()
+        assertEquals(8, app.items[0].sellIn)
     }
 
+    @Test
+    fun decreaseQuality() {
+        val items = arrayOf(Item("random product", 10, 5))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(4, app.items[0].quality)
+        app.updateQuality()
+        assertEquals(3, app.items[0].quality)
+    }
+
+    @Test
+    fun decreaseQualityAfterSellIn() {
+        val items = arrayOf(Item("random product", 0, 6))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(4, app.items[0].quality)
+        app.updateQuality()
+        assertEquals(2, app.items[0].quality)
+    }
+
+    @Test
+    fun qualityIsNeverNegative() {
+        val items = arrayOf(Item("random product", 0, 1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
+    }
+
+    @Test
+    fun maximumQuality() {
+        val items = arrayOf(Item("random product", 12, 100))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(50, app.items[0].quality)
+    }
+
+    @Test
+    fun backstageTAFKAL80ETCQualityIncreases10DaysBeforeSellIn() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 11, 40))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(41, app.items[0].quality)
+        assertEquals(10, app.items[0].sellIn)
+        app.updateQuality()
+        assertEquals(43, app.items[0].quality)
+        assertEquals(9, app.items[0].sellIn)
+    }
+
+    @Test
+    fun backstageTAFKAL80ETCQualityIncreases5DaysBeforeSellIn() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 6, 40))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(42, app.items[0].quality)
+        assertEquals(5, app.items[0].sellIn)
+        app.updateQuality()
+        assertEquals(45, app.items[0].quality)
+        assertEquals(4, app.items[0].sellIn)
+    }
+
+    @Test
+    fun backstageTAFKAL80ETCQualityIsZeroAfterConcert() {
+        val items = arrayOf(Item("Backstage passes to a TAFKAL80ETC concert", 1, 40))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(43, app.items[0].quality)
+        assertEquals(0, app.items[0].sellIn)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
+        assertEquals(-1, app.items[0].sellIn)
+    }
+
+    //- "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches;
+    //	Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+    //	Quality drops to 0 after the concert
 }
 
 
